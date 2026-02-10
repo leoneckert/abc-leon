6fdbff405e56ccf7f5b613f7f62be82ea59c4efa
Leon Eckert <leoneckert@mailbox.org>
2026-02-10 22:37:23 +0800

    s

--- simple-chat-in-class/public/script.js
+++ simple-chat-in-class/public/script.js
@@ -1,7 +1,7 @@
 // let socket = io();
 // socket connection that works locally and on the server:
 if(location.hostname.toLowerCase().startsWith('browsercircus') || location.hostname.toLowerCase().startsWith('www')){
-  socket = io({path: "YOURNAME/port-YOURPORT/socket.io"});  // e.g. '/leon/port-4100/socket.io' or '/socket.io'
+  socket = io({path: "leon/leon-4100/socket.io"});  // e.g. '/leon/port-4100/socket.io' or '/socket.io'
 }else{
   socket = io(); 
 }
