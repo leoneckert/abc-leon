a5fe93df383e831eac1a383a8adbe045ae90d3d4
Leon Eckert <leoneckert@mailbox.org>
2026-02-11 12:35:23 +0800

    s

--- simple-chat-after-class/public/script.js
+++ simple-chat-after-class/public/script.js
@@ -1,4 +1,11 @@
-let socket = io();
+// let socket = io();
+// socket connection that works locally and on the server:
+if(location.hostname.toLowerCase().startsWith('browsercircus') || location.hostname.toLowerCase().startsWith('www')){
+  socket = io({path: "/leon/port-4100/socket.io"});  // e.g. '/leon/port-4100/socket.io' or '/socket.io'
+}else{
+  socket = io(); 
+}
+
 
 let formeElm = document.querySelector("#chatForm");
 console.log(formeElm);
