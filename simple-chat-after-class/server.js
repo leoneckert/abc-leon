9265c826634ecc63a84a3042376d9a4db26cebe9
Leon Eckert <leoneckert@mailbox.org>
2026-02-11 12:00:43 +0800

    changed port sth sth

--- simple-chat-after-class/server.js
+++ simple-chat-after-class/server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 
 const app = express(); // the server "app", the server behaviour
 
-const portHTTPS = 3000; // port for https
+const portHTTPS = 4100; // port for https
 // const portHTTP = 3001; // port for http
 
 
